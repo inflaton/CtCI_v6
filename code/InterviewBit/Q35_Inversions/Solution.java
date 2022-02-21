3325145a40e44808c80effb8896a7bdee1fcc7b7
Donghao Huang <donghao.huang@mastercard.com>
2022-02-21 14:20:24 +0800

    InterviewBit - Q35: added Heapsort + Bisection

--- code/InterviewBit/Q35_Inversions/Solution.java
+++ code/InterviewBit/Q35_Inversions/Solution.java
@@ -4,6 +4,81 @@ import java.util.*;
 
 public class Solution {
 
+  // Heapsort + Bisection
+  static class Pair {
+    int index;
+    int element;
+
+    Pair(int i, int e) {
+      index = i;
+      element = e;
+    }
+
+    public int getIndex() {
+      return index;
+    }
+
+    public int getElement() {
+      return element;
+    }
+
+    @Override
+    public String toString() {
+      return "Pair{" + "index=" + index + ", element=" + element + '}';
+    }
+  }
+
+  public int countInversions(ArrayList<Integer> A) {
+    PriorityQueue<Pair> pq =
+        new PriorityQueue<>(Comparator.comparing(Pair::getElement).thenComparing(Pair::getIndex));
+
+    int i;
+    for (i = 0; i < A.size(); i++) {
+      pq.add(new Pair(i, A.get(i)));
+    }
+
+    int count = 0;
+    int[] sortedIndices = new int[A.size()];
+    int length = 0;
+
+    while (!pq.isEmpty()) {
+      Pair currentMin = pq.remove();
+      // System.out.println(currentMin);
+      // find the current minimum's index
+      i = currentMin.index;
+
+      // the index y can represent how many minimums on the left
+      int y = bisect(sortedIndices, length, i); // O(log(N))
+      // System.out.println("y: " + y + ", length: " + length);
+
+      // i can represent how many elements on the left
+      // i - y can find how many bigger numbers on the left
+      count += i - y;
+
+      if (y < length) {
+        System.arraycopy(sortedIndices, y, sortedIndices, y + 1, length - y);
+      }
+      sortedIndices[y] = i;
+      length++;
+    }
+
+    return count;
+  }
+
+  private int bisect(int[] sortedArray, int length, int v) {
+    int lo = 0;
+    int hi = length;
+    while (lo < hi) {
+      int mid = (lo + hi) / 2;
+      if (sortedArray[mid] >= v) {
+        hi = mid;
+      } else {
+        lo = mid + 1;
+      }
+    }
+    return lo;
+  }
+
   // merge and count
   private static int merge(int[] a, int[] aux, int lo, int mid, int hi) {
     int inversions = 0;
@@ -23,7 +98,9 @@ public class Solution {
       } else if (aux[j] < aux[i]) {
         a[k] = aux[j++];
         inversions += (mid - i + 1);
-      } else a[k] = aux[i++];
+      } else {
+        a[k] = aux[i++];
+      }
     }
     return inversions;
   }
@@ -42,7 +119,7 @@ public class Solution {
     return inversions;
   }
 
-  public int countInversions(ArrayList<Integer> A) {
+  public int countInversionsMergeSort(ArrayList<Integer> A) {
     int[] aux = new int[A.size()];
     int[] a = new int[A.size()];
     for (int i = 0; i < A.size(); i++) {
@@ -119,6 +196,12 @@ public class Solution {
 
     Integer[] s1 = {1, 2, 3, 4, 5};
     runTestCase(s1, 0);
+
+    Integer[] s2 = {
+      84, 2, 37, 3, 67, 82, 19, 97, 91, 63, 27, 6, 13, 90, 63, 89, 100, 60, 47, 96, 54, 26, 64, 50,
+      71, 16, 6, 40, 84, 93, 67, 85, 16, 22, 60
+    };
+    runTestCase(s2, 290);
   }
 
   private static void runTestCase(Integer[] s, int expected) {
