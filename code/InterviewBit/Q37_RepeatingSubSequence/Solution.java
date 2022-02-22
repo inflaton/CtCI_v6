fda0a05e999f3e02ec111b9e0b36258915d9bd4e
Donghao Huang <donghao.huang@mastercard.com>
2022-02-22 22:47:09 +0800

    InterviewBit - Q37 final answers (w/ & w/o recursion)

--- code/InterviewBit/Q37_RepeatingSubSequence/Solution.java
+++ code/InterviewBit/Q37_RepeatingSubSequence/Solution.java
@@ -1,29 +1,41 @@
 package Q37_RepeatingSubSequence;
 
+import java.util.Arrays;
+
 public class Solution {
   public int anytwo(String A) {
     return findLongestRepeatingSubSeq(A) >= 2 ? 1 : 0;
   }
 
-  int findLongestRepeatingSubSeqNonRecursive(String A) {
+  private int findLongestRepeatingSubSeq(String A) {
     int n = A.length();
-    int dp[][] = new int[n + 1][n + 1];
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= n; j++) {
-        if (A.charAt(i - 1) == A.charAt(j - 1) && i != j) {
-          dp[i][j] = 1 + dp[i - 1][j - 1];
-        } else {
-          dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-        }
-      }
+    int[][] dp = new int[n + 1][n + 1];
+    for (int[] d : dp) {
+      Arrays.fill(d, -1);
     }
 
-    return dp[n][n];
+    return findLongestRepeatingSubSeq(A, n, n, dp);
   }
 
-  int findLongestRepeatingSubSeq(String A) {
+  private int findLongestRepeatingSubSeq(String a, int m, int n, int[][] dp) {
+    if (dp[m][n] < 0) {
+      if (m == 0 || n == 0) {
+        dp[m][n] = 0;
+      } else if (a.charAt(m - 1) == a.charAt(n - 1) && m != n) {
+        dp[m][n] = findLongestRepeatingSubSeq(a, m - 1, n - 1, dp) + 1;
+      } else {
+        dp[m][n] =
+            Math.max(
+                findLongestRepeatingSubSeq(a, m, n - 1, dp),
+                findLongestRepeatingSubSeq(a, m - 1, n, dp));
+      }
+    }
+    return dp[m][n];
+  }
+
+  private int findLongestRepeatingSubSeqNonRecursive(String A) {
     int n = A.length();
-    int dp[][] = new int[n + 1][n + 1];
+    int[][] dp = new int[n + 1][n + 1];
     for (int i = 1; i <= n; i++) {
       for (int j = 1; j <= n; j++) {
         if (A.charAt(i - 1) == A.charAt(j - 1) && i != j) {
@@ -59,5 +71,7 @@ public class Solution {
     if (result != expected) {
       throw new AssertionError("result: " + result + " does not equal to expected: " + expected);
     }
+    result = solution.findLongestRepeatingSubSeqNonRecursive(a);
+    System.out.println("non recursive result: " + result);
   }
 }
